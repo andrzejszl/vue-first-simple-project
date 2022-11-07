fec4508dde75d536d6fe593b41aafc2e3ab3ae73
andrzejszl <andrzej.sz123@gmail.com>
2022-11-07 20:09:59 +0100

    first commit

---
+++ src/components/resources/StoredResources.vue
@@ -0,0 +1,23 @@
+<template>
+    <ul>
+        <resource-card v-for="item in items" :key="item.id" :item="item"></resource-card>
+    </ul>
+</template>
+
+<script>
+import ResourceCard from './ResourceCard.vue';
+    export default {
+        components: {ResourceCard},
+        props: ['items']
+    }
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
